d25af028cc1f1cea6e2a6c8aac79ad3eac7d4d60
wildgerecht <info@wildgerecht.de>
2021-04-09 16:57:06 +0200

    major changes

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,17 +5,10 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const NotFoundPage = ({ page, frontPage, data, location }) => {
-  const siteTitle = data.site.siteMetadata.title
-
   const mobilemenu = frontPage.mobileMenu.mobilemenu
 
   return (
-    <Layout
-      location={location}
-      title={siteTitle}
-      uri={page.uri}
-      mobilemenu={mobilemenu}
-    >
+    <Layout location={location} uri={page.uri} mobilemenu={mobilemenu}>
       <SEO title="404: Not Found" />
       <h1>404: Not Found</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
