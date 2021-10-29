273cff2a3e2bdd3ed84d234e6d35bef3b9b9a567
wildgerecht <info@wildgerecht.de>
2021-10-29 10:23:32 +0200

    fix justtext component

--- src/components/sections/JustText.js
+++ src/components/sections/JustText.js
@@ -18,50 +18,32 @@ const Background = styled.div`
   } */
 `
 
-const GoogleMapsWrapper = styled.div`
-  flex: 0 1 auto;
-  width: 100%;
-  position: relative;
-  overflow: hidden;
-  height: 600px;
-  .mappreview {
-    height: 100%;
-    img {
-      object-fit: cover;
-    }
+const Wrapper = styled.div`
+  max-width: ${mq.maxWidth5xl};
+  margin: 0 auto;
+  /* margin: 1rem auto 3rem; */
+  /* margin: var(--spacing-auto); */
+  .textcontent {
+    padding: 0 1rem;
   }
-  picture {
-    opacity: 0.4;
-  }
-  p {
-    font-size: 1rem;
-    font-weight: bold;
-    a {
-      font-size: 1.1rem;
-      font-weight: bold;
-    }
-  }
-  .ladenbutton {
-    border: none;
-    box-shadow: none;
-    &:focus,
-    &:hover {
-      cursor: pointer;
-    }
-  }
-  .maptext {
-    position: absolute;
-    top: 0;
-    left: 0;
-    margin: 0 auto;
-    width: 100%;
-    height: 100%;
-    color: #fff;
-    text-align: center;
-    padding: 220px 1rem;
-    background: rgba(0, 0, 0, 0.2);
-    line-height: 1.6;
-  }
+  padding-bottom: 4rem;
+
+  /* BREAK LONG LINKS */
+  /* These are technically the same, but use both */
+  overflow-wrap: break-word;
+  word-wrap: break-word;
+
+  -ms-word-break: break-all;
+  /* This is the dangerous one in WebKit, as it breaks things wherever */
+  word-break: break-all;
+  /* Instead use this non-standard one: */
+  word-break: break-word;
+
+  /* Adds a hyphen where the word breaks, if supported (No Blink) */
+  -ms-hyphens: auto;
+  -moz-hyphens: auto;
+  -webkit-hyphens: auto;
+  hyphens: auto;
 `
 
 const JustText = ({ sectionid, text, settings, download }) => {
@@ -105,56 +87,31 @@ const JustText = ({ sectionid, text, settings, download }) => {
     backgroundcolor = "orangebackground"
   }
 
-  function handleClick(e) {
-    e.preventDefault()
-
-    // Create Element.remove() function if not exist
-    if (!("remove" in Element.prototype)) {
-      Element.prototype.remove = function () {
-        if (this.parentNode) {
-          this.parentNode.removeChild(this)
-        }
-      }
-    }
-
-    var maptext = document.getElementById("maptext")
-    maptext.remove()
-    var div = document.getElementById("mapsframe")
-    div.firstElementChild.remove()
-    var gmapiframe =
-      "<iframe id='googlemaps' title='Google Maps Wildgerecht' width='100%' height='600px' src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3260.7327314509503!2d11.42896131609896!3d49.98338997941369!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a19bc74f491315%3A0xe53cb1349cdca933!2sPleofen8!5e1!3m2!1sde!2sde!4v1620311130149!5m2!1sde!2sde' frameborder='0' allowfullscreen />"
-    div.innerHTML += gmapiframe
-  }
-
   return (
     <Background id={sectionid} className={spacingtop + " " + spacingbottom}>
       <div className={"midwrap " + backgroundcolor}>
-        <GoogleMapsWrapper id="mapsframe">
-          <StaticImage
-            className="mappreview"
-            src="../../images/mappreview.png"
-            alt="Google Maps Vorschaubild des Wildgerecht Standorts"
-            height={600}
-            style={{ width: "100%" }}
-          />
-          <div id="maptext" className="maptext">
-            <p>
-              Mit dem Laden der Karte akzeptieren
-              <br /> Sie die{" "}
+        <Wrapper id={sectionid}>
+          <div className="textcontent">
+            {!!text && <>{parse(text)}</>}
+            {download && (
               <a
-                href="https://policies.google.com/privacy"
-                rel="noopener noreferrer nofollow"
-                target="_blank"
+                className="button downloadbutton"
+                href={download.localFile.publicURL}
+                download
               >
-                Datenschutzerklärung
-              </a>{" "}
-              von Google.
-            </p>
-            <button className="button ladenbutton" onClick={handleClick}>
-              Karte laden
-            </button>
+                <StaticImage
+                  src="../../images/WG-Download_RGB_orange mit weß.svg"
+                  alt=""
+                  width={32}
+                  height={40}
+                />{" "}
+                <span className="buttontext">
+                  {download.title} ({download.localFile.prettySize})
+                </span>
+              </a>
+            )}
           </div>
-        </GoogleMapsWrapper>
+        </Wrapper>
       </div>
     </Background>
   )
